d447b3db1ef6da56ab0188efe49f153cb91daa09
vinuspread <vinus@vinus.co.kr>
2026-07-06 17:25:13 +0900

    feat: add OHMT001-fashion template (en)

--- src/app/en/templates/OHMT001-fashion/layout.tsx
+++ src/app/en/templates/OHMT001-fashion/layout.tsx
@@ -35,7 +35,6 @@ export default function FashionLayout({
     <>
       <style>{`
         @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
-        @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;700&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400;0,700;1,400&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap');
       `}</style>
